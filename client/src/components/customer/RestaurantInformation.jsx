34a728ccbd127462d44c7cb1639656b4bc7842e5
Lynne Daniels <lynne33477@gmail.com>
2017-10-12 22:55:05 -0700

    display and style Yelp Info on restaurant component

--- client/src/components/customer/RestaurantInformation.jsx
+++ client/src/components/customer/RestaurantInformation.jsx
@@ -15,8 +15,9 @@ const RestaurantInformation = (props) => {
   let waitTime = <div className="restaurant-wait-time"><i className="tiny material-icons">access_time</i> {props.restaurant.total_wait - props.restaurant.average_wait} mins</div>;
   props.restaurant.status === 'Closed' ? statusCircle = closedStatusCircle : statusCircle = openStatusCircle;
   props.restaurant.status === 'Closed' ? waitTime = undefined : waitTime;
-  
+
   return (
+
     <div className="restaurant-info-container row">
       <div className="restaurant-info col s12 m6 l4">
         <div className="restaurant-name">{props.restaurant.name}</div>
@@ -25,8 +26,20 @@ const RestaurantInformation = (props) => {
           <div className="restaurant-queue-status"><span className="status-circle" style={statusCircle}/>{props.restaurant.status}</div>
         </div>
       </div>
-      <div className="col l4 hide-on-med-and-down">address/phone</div>
-      <div className="col l4 m6 hide-on-small-only">yelp stuff</div>
+      <div className="col l4 hide-on-med-and-down yelp-info">
+        <p>{props.restaurant.location.display_address[0]}</p>
+        <p>{props.restaurant.location.display_address[1]}</p>
+        <p>{props.restaurant.phone}</p>        
+      </div>
+      <div className="col l4 m6 hide-on-small-only yelp-info">
+
+        {/*TODO sort out serving img.<a href={props.restaurant.url}><img src="./../images/Yelp_trademark_RGB_outline.png" alt=""/></a>*/}
+        <a href={props.restaurant.yelpURL}><img className="yelp-logo" src="https://s3-media3.fl.yelpcdn.com/assets/srv0/styleguide/b62d62e8722a/assets/img/brand_guidelines/yelp_fullcolor_outline@2x.png" alt=""/></a>
+        {/*TODO - could use entire array of categories?*/}
+        <p>{props.restaurant.categories[0].title}</p>
+        <p>Prices - {props.restaurant.price}</p>
+        <p>Yelp Rating - {props.restaurant.rating}</p>
+      </div>
     </div>
   );
 };
